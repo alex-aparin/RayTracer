013bc73299829e77a803ccc6c4077d4730a759aa
AlexAkel <cool.ipatovo@gmail.com>
2023-02-16 09:02:23 +0300

    Working with light was moved to ray trace module

--- src/ray_tracer.c
+++ src/ray_tracer.c
@@ -1,10 +1,123 @@
 #include "ray_tracer.h"
 #include "graphical_object.h"
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
 
 static float view_port_w = 1;
 static float view_port_h = 1;
 #define GRAPHICAL_OBJECTS_COUNT 3
 #define LIGHT_OBJECTS_COUNT 2
+
+typedef struct
+{
+    float intensity;
+} ambient_light_object;
+
+typedef struct
+{
+    world_point location;
+    float intensity;
+} point_light_object;
+
+typedef struct
+{
+    world_point direction;
+    float intensity;
+} directed_light_object;
+
+void destroy_light_object(void* light_object)
+{
+    free(light_object);
+}
+
+float compute_diffuse_light(const world_point light_direction, const material_t material, const float intensity)
+{
+    const float cos_a = scalar_product(material.normal, light_direction) / length(material.normal) / length(light_direction);
+    if (cos_a <= 0.0f)
+        return 0.0f;
+    return cos_a * intensity;
+}
+
+float compute_specular_light(const world_point light_direction, const material_t material, const float intensity, const world_point view_vector)
+{
+    if (material.specularity == -1)
+        return 0.0f;
+    const world_point reflected_light_dir = sub(mul_by_factor(material.normal, 2.0f * scalar_product(light_direction, material.normal)), light_direction);
+    const float cos_a = scalar_product(reflected_light_dir, mul_by_factor(view_vector, -1.0f)) / length(reflected_light_dir) / length(view_vector);
+    if (cos_a <= 0.0f)
+        return 0.0f;
+    return (float)pow(cos_a, (float)material.specularity) * intensity;
+}
+
+float ambient_light_intensity(void* instance, const world_point point, const material_t material, const world_point view_vector)
+{
+    return ((ambient_light_object*)instance)->intensity;
+}
+
+float point_light_intensity(void* instance, const world_point point, const material_t material, const world_point view_vector)
+{
+    const point_light_object* const point_light = (point_light_object*)instance;
+    const world_point light_dir = sub(point_light->location, point);
+    const float intensity = point_light->intensity;
+    return compute_diffuse_light(light_dir, material, intensity) + compute_specular_light(light_dir, material, intensity, view_vector);
+}
+
+float directed_light_intensity(void* instance, const world_point point, const material_t material, const world_point view_vector)
+{
+    const directed_light_object* const directed_light = (directed_light_object*)instance;
+    const world_point light_dir = mul_by_factor(directed_light->direction, -1);
+    const float intensity = directed_light->intensity;
+    return compute_diffuse_light(light_dir, material, intensity) + compute_specular_light(light_dir, material, intensity, view_vector);
+}
+
+light_object create_ambient_light(float intensity_value)
+{
+    light_object light;
+    ambient_light_object* ambient_light = malloc(sizeof(ambient_light_object));
+    ambient_light->intensity = intensity_value;
+    light.instance = ambient_light;
+    light.intensity_func = ambient_light_intensity;
+    light.destroy_func = destroy_light_object;
+    return light;
+}
+
+light_object create_point_light(const world_point location, float intensity)
+{
+    light_object light;
+    point_light_object* point_light = malloc(sizeof(point_light_object));
+    point_light->location = location;
+    point_light->intensity = intensity;
+    light.instance = point_light;
+    light.intensity_func = point_light_intensity;
+    light.destroy_func = destroy_light_object;
+    return light;
+}
+
+light_object create_directed_light(const world_point direction, float intensity)
+{
+    light_object light;
+    directed_light_object* directed_light = malloc(sizeof(directed_light_object));
+    directed_light->direction = direction;
+    directed_light->intensity = intensity;
+    light.instance = directed_light;
+    light.intensity_func = directed_light_intensity;
+    light.destroy_func = destroy_light_object;
+    return light;
+}
+
+float compute_light_intensity(light_object* const lights, const int count, const world_point point, const material_t material, const world_point view_vector)
+{
+    if (count == 0)
+        return 1.0f;
+    float intensity = 0.0f;
+    for (int i = 0; i < count; ++i)
+    {
+        intensity += lights[i].intensity_func(lights[i].instance, point, material, view_vector);
+    }
+    return MIN(intensity, 1.0f);
+}
+
 
 world_point viewport_point(screen_point p, const int canvas_width, const int canvas_height)
 {
@@ -24,7 +137,7 @@ world_point from_viewport(world_point p, const int canvas_width, const int canva
     return res;
 }
 
-int find_nearest_object_intersection(const world_line line, graphic_object* objects, int count, float* t)
+int find_nearest_object_intersection(const world_line line, graphic_object* objects, int count, float tmin, float tmax, float* t)
 {
     int object_index = -1;
     for (int i = 0; i < count; ++i)
@@ -35,7 +148,7 @@ int find_nearest_object_intersection(const world_line line, graphic_object* obje
             continue;
         for (int root_index = 0; root_index < roots_count; ++root_index)
         {
-            if (roots[root_index] < 1.0f)
+            if (roots[root_index] < tmin || tmax < roots[root_index])
                 continue;
             if (object_index == -1 || roots[root_index] < *t)
             {
@@ -91,7 +204,7 @@ void trace(const int canvas_width, const int canvas_height, put_pixel_callback p
             zero(&line.origin);
             line.dir = p;
             float t = 0;
-            int object_index = find_nearest_object_intersection(line,  graphical_objects, GRAPHICAL_OBJECTS_COUNT, &t);
+            int object_index = find_nearest_object_intersection(line,  graphical_objects, GRAPHICAL_OBJECTS_COUNT, 1.0f, FLT_MAX, &t);
             if (object_index != -1)
             {
                 const world_point surface_point = line_point(line, t);
